23a67fdcb05b3e2cad2f032aa20818f36410f06f
zfphoenix <zfphoenix@163.com>
2015-07-19 02:07:49 +0800

    upload

--- src/container.h
+++ src/container.h
@@ -2,7 +2,15 @@
 #define KEYVALUE_H_
 
 #include <string>
-#include "log.h"
+#include <map>
+#include <vector>
+
+extern "C" {
+#include <stdint.h>
+}
+
+class Redis;
+
 class Container {
 public:
 	Container(Redis* redis) {
@@ -12,44 +20,88 @@ public:
 		release();
 	}
 	
-	void release() {
-		if (redis_->Reply()) {
-				freeReplyObject(redis_->Reply());
-		}
-	}
+	void release();
 
 	//after operate on db and invoke this func 
-	bool check_status() {
- 		if(redis_->Connect()->err)  {
-			MLOG->Error("Connect ERR err is %d", redis_->Connect()->err);
-			return false;
-		}
-		else if (!redis_->Reply()){
-			MLOG->Error("ERR empty reply");
-			return false;
-		} else if (REPLY_TYPE_ERROR == redis_->Reply()->type) {
-			MLOG->Error("Reply ERR err is %s", redis_->Reply()->str);
-		} else {
-			return true;
-		}
-	}
-
+	bool check_status();
+ 	
 private:
 	Redis *redis_;
 
 };
 
-class KeyValue : public Container {
+//KeyValue class used to manipulate normal
+//[key-value] data structure
+class RedString : public Container {
 public:
-	KeyValue(Redis* redis):Container(redis) {
+	typedef std::string								Key;
+	typedef std::string								Value;
+	typedef std::vector< std::string >				MKey;
+	typedef std::vector< std::string >				MValue;
+	typedef MKey::iterator							MKeyItr;
+	typedef MValue::iterator						MValueItr;
+	typedef std::vector< std::pair< Key, Value > >	MKValue;
+	typedef MKValue::iterator					    MKValueItr;
+public;
+	RedString(Redis* redis):Container(redis) {
 
 	}
 	
-	bool Set(std::string key, std::string value);
-
-	bool Get(std::string key);
-
+	//set key value if not exist
+	//else update key's value with 'v'
+	bool Set(Key k, Value v);
+	//get key's value if exist
+	Value Get(Key k);
+	//set if not exists
+	bool SetNx(Key k, Value v);
+	//multi set
+	bool MSet(MKValue mkv);
+	//multi get
+	MValue MGet(MKey mk);
+	//multi set if not exists
+	bool MSetNx(MKValue mkv);
+	//if k's value exists, update value with new_v
+	//else set [k - new_v]
+	bool GetSet(Key k, Value new_v);
+	//change key's value if exist
+	//and replace part value with 'replace' string start from 
+	//index-th char
+	bool SetRange(Key k, uint32_t index, Value replace);
+	//return part key's value which in range[start, end]
+	Value GetRange(Key k, uint32_t start, uint32_t end);
+	//update k'value and append value with 'append'
+	bool Append(Key k, Value append);
+	//return key's value's length,unit is byte
+	size_t StrLen(Key k);
+	//!!!
+	//!!!
+	//!!!below funcs were only invoked while key's value is type-Integer
+	//increase key's value with 1
+	// -1 represent decrease 1
+	bool Incr(Key k);
+	//increase key's value with num
+	bool IncrBy(Key k, uint32_t num);
+	//decrease key's value with 1
+	//-1 represent increase 1
+	bool Decr(Key k);
+	//decrease key's value with num
+	bool DecrBy(Key k, uint32_t num);
 };
 
+//Hash Key : field1-value1 field2-value2 field3-value3....
+class RedHash : public Container {
+public:
+	typedef	std::string								 Key;
+	typedef std::string								 Field;
+	typedef std::string								 Value;
+	typedef std::vecotr< Key >						 FieldSet;
+	typedef FieldSet::iterator						 FieldSetItr;
+	typedef FieldSet								 ValueSet;
+	typedef FieldSetItr								 ValueSetItr;
+	typedef std::vector< std::pair< Field, Value > > Dict;
+	typedef Dict::iterator							 DictItr;
+	typedef Dict									 MFValue;
+	typedef DictItr									 MFValueItr;
+};
 
 #endif
